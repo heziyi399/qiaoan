7d1684cf72d0acfb4c548265a34b2f97363e24d5
heziyi <1417243391@qq.com>
2022-04-09 20:28:56 +0800

    qiaoanproject

---
+++ src/main/java/com/qiaoan_science/QiaoanScienceApplication.java
@@ -0,0 +1,13 @@
+package com.qiaoan_science;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiaoanScienceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QiaoanScienceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qiaoan_science/QiaoanScienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiaoan_science;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiaoanScienceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
